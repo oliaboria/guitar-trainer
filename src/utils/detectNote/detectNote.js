fb802f2bea3cad3726cf9bd816855e238bca97f9
Volha Borys <olgaboris94@gmail.com>
2020-09-24 13:22:52 +0300

    refactor: fix lint errors

--- src/utils/detectNote/detectNote.js
+++ src/utils/detectNote/detectNote.js
@@ -1,14 +1,16 @@
 const NOTES = ['C', 'C#', 'D', 'D#', 'E', 'F', 'F#', 'G', 'G#', 'A', 'A#', 'B'];
 const NUMBER_OF_NOTES = NOTES.length;
 
-const c0 = 440.0 * 2 ** -4.75;
+const C0 = 440.0 * 2 ** -4.75;
 
 const detectNote = (frequency) => {
     if (!frequency) return null;
 
-    const halfStepsBelowMiddleC = Math.round(12.0 * Math.log2(frequency / c0));
-    const octave = Math.floor(halfStepsBelowMiddleC / NUMBER_OF_NOTES);
-    const key = NOTES[Math.floor(halfStepsBelowMiddleC % NUMBER_OF_NOTES)];
+    const noteFromBaseC = Math.round(
+        NUMBER_OF_NOTES * Math.log2(frequency / C0),
+    );
+    const octave = Math.floor(noteFromBaseC / NUMBER_OF_NOTES);
+    const key = NOTES[Math.floor(noteFromBaseC % NUMBER_OF_NOTES)];
 
     return { key, octave };
 };
