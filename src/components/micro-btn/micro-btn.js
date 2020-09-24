85a417d8feff77f450d3c7fcbe2b0343ce298402
Volha Borys <olgaboris94@gmail.com>
2020-09-24 12:57:30 +0300

    get pitch of the sound

---
+++ src/components/micro-btn/micro-btn.js
@@ -0,0 +1,60 @@
+import { PitchDetector } from 'pitchy';
+
+class MicroBtn extends HTMLElement {
+    #root;
+    #btnEl;
+    #isEnabled;
+    audioContext;
+    analyserNode;
+    #userMedia;
+
+    constructor() {
+        super();
+        this.#root = this.attachShadow({ mode: 'open' });
+        this.#isEnabled = false;
+    }
+
+    connectedCallback() {
+        this.#btnEl = document.createElement('button');
+        this.#btnEl.innerText = 'Microphone';
+
+        this.#root.appendChild(this.#btnEl);
+
+        this.#userMedia = navigator.mediaDevices.getUserMedia({ audio: true });
+
+        this.#btnEl.addEventListener('click', () => {
+            if (this.#isEnabled) {
+                this.audioContext.close();
+                this.#isEnabled = false;
+            } else {
+                this.audioContext = new (window.AudioContext ||
+                    window.webkitAudioContext)();
+                this.analyserNode = this.audioContext.createAnalyser();
+
+                this.#userMedia.then((stream) => {
+                    this.#isEnabled = true;
+                    const sourceNode = this.audioContext.createMediaStreamSource(
+                        stream,
+                    );
+                    sourceNode.connect(this.analyserNode);
+                    setTimeout(() => {
+                        const detector = PitchDetector.forFloat32Array(
+                            this.analyserNode.fftSize,
+                        );
+                        const input = new Float32Array(detector.inputLength);
+                        const { sampleRate } = this.audioContext;
+
+                        this.analyserNode.getFloatTimeDomainData(input);
+
+                        const [pitch, clarity] = detector.findPitch(
+                            input,
+                            sampleRate,
+                        );
+                    }, 1000);
+                });
+            }
+        });
+    }
+}
+
+export default MicroBtn;
